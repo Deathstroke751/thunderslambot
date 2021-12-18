692fe0d930dea6f19b269cbb5989f9dd0e50e4fb
lqxrt <76816832+lqxrt@users.noreply.github.com>
2021-12-18 13:06:12 +0530

    Remove redundant checks from alive.py

--- alive.py
+++ alive.py
@@ -7,13 +7,8 @@ import requests
 import os
 
 BASE_URL = os.environ.get('BASE_URL_OF_BOT', None)
-try:
-    if len(BASE_URL) == 0:
-        BASE_URL = None
-except:
-    BASE_URL = None
 PORT = os.environ.get('PORT', None)
-if PORT is not None and BASE_URL is not None:
+if PORT and BASE_URL:
     while True:
         time.sleep(600)
         status = requests.get(BASE_URL).status_code
