be3fce3a2d42b2f9415fb5e6a6e07cd20ff38f82
lqxrt <76816832+lqxrt@users.noreply.github.com>
2021-12-26 11:05:37 +0530

    Fix shell access

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -39,5 +39,5 @@ def shell(update, context):
 
 
 SHELL_HANDLER = CommandHandler(BotCommands.ShellCommand, shell,
-                               filters=CustomFilters.sudo_user, run_async=True)
+                               filters=CustomFilters.sudo_user | CustomFilters.mirror_owner_filter, run_async=True)
 dispatcher.add_handler(SHELL_HANDLER)
